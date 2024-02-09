49bd1a659584e698773ad0541b2afd6ca62d8495
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2024-02-09 15:04:03 +0300

    navbar wont appear on error page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "../styles/Navbar.css";
 import pages from "../pages/pages.json";
 
 export const Navbar = () => {
+  const { pathname } = useLocation();
+
+  if (pathname === "/error") {
+    return <></>;
+  }
   return (
     <div className="navbar">
       <div className="links-container">

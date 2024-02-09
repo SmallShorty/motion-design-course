0867455b8c6bc7c0515c6fd52ea345ed82558ded
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2024-02-09 15:48:13 +0300

    updated error page layout in Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export const Error = () => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/home");
+  }
+
   return (
-    <div>
-      <p>Oops! Something went wrong.</p>
+    <div className="message-container">
+      <span>400</span>
+      <p>Oops! Something went wrong. . .</p>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 };
